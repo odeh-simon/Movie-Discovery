b2eeb651c463ad43472e2022fb57204aea548ab7
Odeh Simon <simonodeh36@gmail.com>
2023-09-17 21:36:34 +0100

    Update MovieCard.js

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import './App.css';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
 
 function MovieCard({ movie }) {
   const [isRed, setIsRed] = useState(false);
@@ -13,24 +14,29 @@ function MovieCard({ movie }) {
   };
 
   return (
-    <Link to={`/movies/${movie.id}`} style={{ textDecoration: 'none', color: 'black' }}>
-      <div className="movie-card" data-testid="movie-card">
-        <div className="movie-poster-container">
-          <img src={movie.poster} alt={movie.title} data-testid="movie-poster" />
-     <button
-            className="favorite-button"
-            onClick={(e) => handleFavoriteClick(e)}
-            style={{ color: isRed ? 'red' : 'white' }}
-          >
-            <FontAwesomeIcon icon={faHeart} />
-          </button>
-          <p data-testid="movie-release-date">{movie.release_date}</p>
-          <h2 data-testid="movie-title">{movie.title}</h2>
-       
+<div>
+    <div className="movie-card" data-testid="movie-card">
+         <Link to={`/movies/${movie.id}`} style={{ textDecoration: 'none', color: 'black' }}>
+      <div className="movie-poster-container">
+        <img src={movie.poster} alt={movie.title} data-testid="movie-poster" />
+        <p data-testid="movie-release-date">{movie.release_date}</p>
+        <h2 data-testid="movie-title">{movie.title}</h2>
         </div>
-      </div>
-    </Link>
+        </Link>
+      
+        <button
+          className={`favorite-button${isRed ? ' favorite' : ''}`}
+          onClick={(e) => handleFavoriteClick(e)}
+          style={{ color: isRed ? 'red' : 'white' }}
+        >
+          <FontAwesomeIcon icon={faHeart} />
+        </button>
+        </div>
+        </div>
+     
+    
+    
   );
-}
+};
 
 export default MovieCard;
